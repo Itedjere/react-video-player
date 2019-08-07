4de7f67133f2d4b323f3d8ba24a0fd9af4f06334
Itedjere Godspower <itedjereg77@gmail.com>
2019-08-07 17:58:32 +0100

    first commit

---
+++ src/components/Nightmode.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StyledNightMode from './styles/StyledNightmode';
+
+const NightMode = ({ nightMode, nightModeCallback }) => (
+    <StyledNightMode>
+        <span>Nightmode: </span>
+        <label className="switch">
+            <input type="checkbox" checked={nightMode} onChange={nightModeCallback} />
+            <span className="slider round" />
+        </label>
+    </StyledNightMode>
+);
+
+export default NightMode;
---
+++ src/components/containers/Playlist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PlaylistHeader from '../PlaylistHeader';
+import PlaylistItems from './PlaylistItems';
+import NightMode from '../Nightmode';
+import StyledPlaylist from '../styles/StyledPlaylist';
+
+const PlayList = ({ videos, active, nightModeCallback, nightMode }) => (
+    <StyledPlaylist>
+        <NightMode nightModeCallback={nightModeCallback} nightMode={nightMode} />
+        <PlaylistHeader active={active} total={videos.length} />
+        <PlaylistItems videos={videos} active={active} />
+    </StyledPlaylist>
+)
+
+export default PlayList;
